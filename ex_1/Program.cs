24ee20f02a2bcea8950942a61dfd05bcd7308a88
Best Cat <kotova-irina11@rambler.ru>
2022-09-25 20:30:17 +0300

    задача с семинара, поменять строки местами

---
+++ ex_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 54: Поменять 1 и последнюю строку двумерного массива
+
+int rows = 7; // кол-во строк
+int colums = 6; // кол-во столбцов
+int[,] array = GetArray(rows, colums, 10,20);
+int[,] GetArray(int m,int n, int minValue,int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue + 1); 
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+     for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+           Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] SelectionSort(int[,]array)
+{
+    int rows = array.GetLength(0);
+    int colums = array.GetLength(1);
+    int[] TempArr = new int[rows];
+    for (int i = 0; i < colums; i++)
+    {
+    TempArr[i] =  array[0,i];
+    array[0,i] = array[rows -1,i];
+    array[rows -1,i] =  TempArr[i];
+    }
+    return array;
+}
+    
+    //}
+    //}
+PrintArray(array);
+SelectionSort(array);
+Console.WriteLine();
+PrintArray(array);
