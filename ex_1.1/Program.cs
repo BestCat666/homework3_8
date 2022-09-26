b279de7fd6a4dd5383a646a9699b782bbf2bd52e
Best Cat <kotova-irina11@rambler.ru>
2022-09-26 19:34:51 +0300

    задача 54, упорядочить строки массива

--- ex_1.1/Program.cs
+++ ex_1.1/Program.cs
@@ -8,9 +8,9 @@
 //7 4 2 1
 //9 5 3 2
 //8 4 4 2
-int rows = 4; // кол-во строк
-int colums = 4; // кол-во столбцов
-int[,] array = GetArray(rows, colums, 10,20);
+int rows = 5; // кол-во строк
+int colums = 5; // кол-во столбцов
+int[,] array = GetArray(rows, colums, 1,30);
 int[,] GetArray(int m,int n, int minValue,int maxValue)
 {
     int[,] result = new int[m,n];
@@ -39,8 +39,9 @@ int [,] SelectionSort(int[,]array)
 {
     int rows = array.GetLength(0);
     int colums = array.GetLength(1);
-    int min = 0;
     //int[,] TempArr = new int[rows,colums];
+    for (int count = 0; count < (rows * colums - 1) ; count++)
+    {  
     for (int i = 0; i < rows; i++)
     {
     for (int j = 0; j < colums-1; j++)
@@ -50,17 +51,15 @@ int [,] SelectionSort(int[,]array)
     //for (int m = j + 1; m < colums/2; m++)
     //{  
     //TempArr[k,m] = array[i,j];
-    for (int count = 0; count < colums*rows; count++)
-    {  
-    if(array[i,j] <= array[i,j +1])  
+    if(array[i,j] < array[i,j + 1])
     {
-    min = array[i,j];
+    int temp = array[i,j];
     array[i,j] = array[i,j+1];
-    array[i,j+1] = min;
+    array[i,j+1] =temp;
     }
     }
     }
-    }  
+    } 
     return array; 
 }
 
